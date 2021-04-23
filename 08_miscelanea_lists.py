f37142b978f640b26165597322f176c5e27d5759
ronikode <rnoboa22@gmail.com>
2021-04-22 19:15:44 -0500

    feat: adding the first task

---
+++ 08_miscelanea_lists.py
@@ -0,0 +1,19 @@
+"""
+4.-) La lista mostrada en el ejemplo contiene URLs de diferentes sitios web que han sido visitados.
+Las URLs normalmente se repiten y corresponden algunas veces a universidades de Ecuador y otros países.
+Note que los URLs no diferencian entre mayúsculas y minúsculas.
+
+Escriba un programa en python que realice lo siguiente:
+
+A. Muestre los nombres o siglas de las universidades que aparecen en la lista (sin repetir).
+B. Muestre la cantidad y nombres/siglas de universidades de Ecuador que aparecen en la lista.
+
+"""
+
+universities = [
+    "www.espol.edu.ec", "www.google.com", "www.sri.gob.ec", "www.fiec.espol.edu.ec",
+    "www.uees.edu.ec", "www.FIEC.espol.edu.ec", "www.fict.espol.edu.ec",
+    "www.fcnm.Espol.edu.ec", "www.ucsg.edu.ec", "www.Standford.edu", "www.harvard.edu",
+    "www.standford.edu", "www.google.com.ec", "www.elnoticiero.com.ec", "www.opensource.org",
+    "www.facebook.com", "www.UCSG.edu.ec"
+]
