9877e20225281c09be50d66e0e3b6330cbe9b850
ronikode <rnoboa22@gmail.com>
2021-04-26 18:28:33 -0500

    feat: task exercise 10

---
+++ 10_task_poo.py
@@ -0,0 +1,13 @@
+"""
+Crear los siguientes objetos:
+
+Customer (visto en la clase pasada)
+
+Category (slug, name, description*)
+
+Item: (sku, name, description*, cost, pvp, category)
+
+Order: (code, date_created, customer, total_products, base_12, base_0, subtotal, total, items=[])
+
+OrderItem: (item, quantity, subtotal)
+"""
